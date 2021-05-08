2249d7ec7cd2ce11b6055d9461ad9e3150fd91f7
metametaclass <metametaclass@gmail.com>
2021-05-08 18:16:35 +0300

    added weight to dump

--- src/export_dump.cc
+++ src/export_dump.cc
@@ -6,34 +6,38 @@
 #include "Polygon2d.h"
 #include "CGAL_Nef_polyhedron.h"
 
+void dump_geometry_header(const shared_ptr<const Geometry> &geom, std::ostream &output, int current_indent)
+{
+	for (int i = 0; i < current_indent; ++i) {
+		output << "\t";
+	}
+	output << "name:" << geom->getName() << " weight:" << geom->getWeight() << " material:" << geom->getMaterialName() << " density:" << geom->getDensity() << " color:"<<geom->getColor() << "\n";
+}
+
 void export_dump_inner(const shared_ptr<const Geometry> &geom, std::ostream &output, int current_indent)
 {
 	for (int i = 0; i < current_indent; ++i) {
 		output << "\t";
 	}
 
-
 	if (const auto geomlist = dynamic_pointer_cast<const GeometryList>(geom)) {
 		output << "GeometryList " << geomlist->getChildren().size() << "\n";
-		output << "\tname:" << geom->getName() << " material:" << geom->getMaterialName() << " density:" << geom->getDensity() << " color:"<<geom->getColor() << "\n";
+		dump_geometry_header(geom, output);
 		for (const Geometry::GeometryItem &item : geomlist->getChildren()) {
 			output << "GeometryItem " << item.first->verbose_name() << " " << item.first->toString() << " " << typeid(*item.second).name() << "\n";
 			export_dump_inner(item.second, output, current_indent+1);
-			// item.first->verbose_name()); triangle_count += append_stl(item.second, output);
 		}
 	}
 	else if (const auto N = dynamic_pointer_cast<const CGAL_Nef_polyhedron>(geom)) {
 		output << "CGAL_Nef_polyhedron " << N->numFacets() << "\n";
-		output << "\tname:" << geom->getName() << " material:" << geom->getMaterialName() << " density:" << geom->getDensity() << " color:"<<geom->getColor() << "\n";
+		dump_geometry_header(geom, output);
 		output << const_cast<CGAL_Nef_polyhedron3&>(*N->p3);
-		// N->numFacets()); triangle_count += append_stl(*N, output);
 	}
 	else if (const auto ps = dynamic_pointer_cast<const PolySet>(geom)) {
 		//output << "PolySet " << ps->polygons.size() << "\n";
 		output << "PolySet\n";
-		output << "\tname:" << geom->getName() << " material:" << geom->getMaterialName() << " density:" << geom->getDensity() << " color:"<<geom->getColor() << "\n";
+		dump_geometry_header(geom, output);
 		output << ps->dump();
-		// triangle_count += append_stl(*ps, output);
 	}
 	else if (dynamic_pointer_cast<const Polygon2d>(geom)) {
 		assert(false && "Unsupported file format");
